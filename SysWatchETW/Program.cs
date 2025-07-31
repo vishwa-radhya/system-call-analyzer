5c37c6c825a3d75eadc4ce978ae101f85f8c4616
vishwa-radhya <vishuvishwa735@gmail.com>
2025-07-31 21:14:47 +0530

    feat: initial system call logger setup using dotnet application with nuget package:Microsoft.Diagnostics.Tracing.TraceEvent, which logs Process-s and FileIOInit and saves to .log file

---
+++ SysWatchETW/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using Microsoft.Diagnostics.Tracing;
+using Microsoft.Diagnostics.Tracing.Parsers;
+using Microsoft.Diagnostics.Tracing.Parsers.Kernel;
+using Microsoft.Diagnostics.Tracing.Session;
+
+
+class Program
+{
+    static void Main()
+    {
+        if (!(TraceEventSession.IsElevated() ?? false))
+        {
+            Console.WriteLine("Please run as Administrator.");
+            return;
+        }
+
+        string logPath = "SysWatchProcessLogs.log";
+        using StreamWriter logFile = new StreamWriter(logPath, append: true);
+
+        using (var session = new TraceEventSession("SysWatchSession"))
+        {
+            Console.CancelKeyPress += (s, e) => session.Dispose();
+
+            session.EnableKernelProvider(
+                KernelTraceEventParser.Keywords.Process 
+                // KernelTraceEventParser.Keywords.FileIOInit
+            );
+            // session.EnableKernelProvider((KernelTraceEventParser.Keywords)(0x10 | 0x40)); // Process + FileIO
+            session.Source.Kernel.ProcessStart += (proc) =>
+            {
+                string msg = $"[+] Process Started: {proc.ProcessName} (PID: {proc.ProcessID})";
+                Console.WriteLine(msg);
+                logFile.WriteLine(msg);
+            };
+
+            session.Source.Kernel.ProcessStop += (proc) =>
+            {
+                string msg = $"[-] Process Exited: {proc.ProcessName} (PID: {proc.ProcessID})";
+                Console.WriteLine(msg);
+                logFile.WriteLine(msg);
+            };
+
+            session.Source.Kernel.FileIOCreate += (file) =>
+            {
+                string msg = $"[FileIO] {file.ProcessName} -> {file.FileName}";
+                Console.WriteLine(msg);
+                logFile.WriteLine(msg);
+            };
+
+            Console.WriteLine("Logging to SysWatch.log ... Press Ctrl+C to stop.");
+            session.Source.Process(); // blocks here
+        }
+    }
+}
